17f90826e88d54cf9b30fd04d7d6fab63bbdbfa7
Joseph <joesyby@gmail.com>
2025-07-03 12:20:55 -0400

    Update main and pipeline

--- src/Main/main.py
+++ src/Main/main.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from Llm_pipeline.pipeline import create_qa_chain, qa_bot_answer, load_llm, custom_prompt, 
+from Llm_pipeline.pipeline import create_qa_chain, qa_bot_answer, load_llm, custom_prompt 
 from Topic_Router.topic_router import topic_to_response
   # Import the topic_to_response mapping
 
